aa5ccfbfe94aba0ad5ae5fc8311e1a9a3a9802b9
xInsanityx1022 <richar.domin@awatapu.school.nz>
2021-09-15 12:06:41 +1200

    Ticket Looping system

---
+++ 02_ticket_loop_v2.py
@@ -0,0 +1,22 @@
+# start of loop
+
+# initialise loop so that it runs at least once
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+    print("You have {} seats "
+          "left".format(MAX_TICKETS - count))
+
+    # Get details...
+    name = input("Name: ")
+    count += 1
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all available tickets!")
+else:
+    print("You have sold {} tickets. \n"
+          "there are {} places still available"
+          .format(count, MAX_TICKETS - count))
