99cfa09663875a414375367041cbc291f12a5fda
xInsanityx1022 <richar.domin@awatapu.school.nz>
2021-09-15 12:08:00 +1200

    Yes No

---
+++ 05_yes_no.py
@@ -0,0 +1,24 @@
+
+def yes_no(question):
+
+    error = "Please answer with yes or no"
+
+    valid = False
+    while not valid:
+
+        # ask question and put response in lowercase
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "no":
+            return "no"
+        else:
+            print(error)
+
+# Main routine goes here
+
+for item in range(0, 6):
+    want_snacks = yes_no("Do you want snacks? ")
+    print("Answer OK, you said:", want_snacks)
+    print()
---
+++ 05_yes_no_v2.py
@@ -0,0 +1,28 @@
+
+# string checking function, takes in question and list of valid responses
+def string_checker(question, to_check):
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        if response in to_check:
+            return response
+
+        else:
+            for item in to_check:
+                # checks if response is the first letter of
+                # an item in the list
+                if response == item[0]:
+                    # note: return the entire response
+                    # rather than just the first letter
+                    return item
+
+        print("Sorry that is not a valid response")
+
+#  *** Main Routine starts here ***
+for item in range(0, 6):
+    want_snacks = string_checker("Do you want snacks? ", ["yes", "no"])
+    print("Answer ok, you said:", want_snacks)
+    print()
