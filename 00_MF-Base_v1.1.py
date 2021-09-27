dfe29080d574a7dbeebd6e0a3732c37c1896e7bb
xInsanityx1022 <richar.domin@awatapu.school.nz>
2021-09-28 10:53:42 +1300

    MAIN BASE

---
+++ 00_MF-Base_v1.1.py
@@ -0,0 +1,30 @@
+# import statements
+
+
+# functions go here
+
+
+# ********** Main Routine **********
+
+# set up dictionary / lists needed to hold data
+
+# ask user if they have used the program before & show instructions
+
+# loop to get ticket details
+
+    # get name (not blank)
+
+    # get age (between 12 & 130)
+
+    # calculate ticket price
+
+    # loop to ask for snacks
+
+    # calculate snack price
+
+    # ask user for payment method (and apply surcharge if nessacary)
+
+
+# calculate total sales and profit
+
+# output data to text file
